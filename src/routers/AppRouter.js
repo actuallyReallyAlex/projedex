92b07d012ddeefb163b800c81b8da31d5dc511b5
Alex Lee <alex@alex-lee.site>
2018-11-27 13:05:47 -0800

    ✨ AppRouter

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,21 +2,28 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Grommet } from 'grommet'
 import { grommet } from 'grommet/themes'
-import { RadialSelected } from 'grommet-icons'
+import Nav from '../components/Nav'
+import Section from '../components/Section'
+import Home from '../pages/Home'
 
-const Home = () => (<RadialSelected size="xlarge" color="neutral-4" />)
-const NotFound = () => (<h1>NotFound</h1>)
 
+const NotFound = () => <h1>NotFound</h1>
 
-const AppRouter = () => (
-  <BrowserRouter>
+grommet.global.colors.brand = "#6194EB"
+
+const AppRouter = () => {
+  console.log(grommet)
+  return ( <BrowserRouter>
     <Grommet theme={grommet}>
+      <Section>
+        <Nav />
+      </Section>
       <Switch>
         <Route path="/" component={Home} exact />
         <Route component={NotFound} />
       </Switch>
     </Grommet>
   </BrowserRouter>
-)
+)}
 
 export default AppRouter
