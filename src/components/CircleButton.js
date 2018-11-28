71b74ec4f9725280da89cd3564ad7dda0280e100
Alex Lee <alex@alex-lee.site>
2018-11-28 11:17:01 -0800

    ✏️ Change CircleButton to SFC
    No need to be a class. Removed old commented out code.

--- src/components/CircleButton.js
+++ src/components/CircleButton.js
@@ -1,39 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Box, Button } from 'grommet'
 
-class CircleButton extends Component {
-  render() {
-    const Icon = this.props.icon
-    const { link, color } = this.props
-
-    return (
-      <Box
-        background={color ? color : 'accent-1'}
-        elevation="medium"
+const CircleButton = ({ icon, link, color }) => {
+  const Icon = icon
+  return (
+    <Box
+      background={color ? color : 'accent-1'}
+      elevation="medium"
+      style={{ borderRadius: '50%' }}
+    >
+      <Button
         style={{ borderRadius: '50%' }}
-      >
-        <Button
-          style={{ borderRadius: '50%' }}
-          hoverIndicator
-          icon={<Icon color="white" />}
-          href={link}
-          target="_blank"
-        />
-      </Box>
-    )
-  }
+        hoverIndicator
+        icon={<Icon color="white" />}
+        href={link}
+        target="_blank"
+      />
+    </Box>
+  )
 }
-
-// const CircleButton = ({ icon }) => (
-//   <Box background="accent-1" elevation="medium" style={{ borderRadius: '50%' }}>
-//     <Button
-//       color="accent-1"
-//       style={{ borderRadius: '50%' }}
-//       hoverIndicator
-//       icon={icon}
-//       onClick={() => alert('clicked')}
-//     />
-//   </Box>
-// )
 
 export default CircleButton
