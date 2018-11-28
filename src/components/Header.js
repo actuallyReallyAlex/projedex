301374ff965ece059295473f2f6005e302f28050
Alex Lee <alex@alex-lee.site>
2018-11-27 18:14:49 -0800

    ✏️ Edit header alignment

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Box, Heading, Paragraph } from 'grommet'
 
 const Header = ({ label, summary }) => (
-  <Box align="center" margin={{ horizontal: 'large' }}>
+  <Box justify="center" align="start" margin={{ horizontal: 'large' }}>
     <Heading level={1} size="large" margin="none">
       {label}
     </Heading>
