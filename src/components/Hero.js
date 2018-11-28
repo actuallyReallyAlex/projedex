804ce37e4c51bbfc11bdf3a6df697787ed943e70
Alex Lee <alex@alex-lee.site>
2018-11-28 07:41:24 -0800

    ✏️ Use Fader in Hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import { ResponsiveContext, Box } from 'grommet'
-import Beer from './svg/Beer'
 import Header from './Header'
+import Fader from './Fader'
+import Beer from './svg/Beer'
+import Dashboard from './svg/Dashboard'
+import Wireframe from './svg/Wireframe'
 
 const Hero = () => (
   <ResponsiveContext.Consumer>
     {size => (
       <Box direction="row">
         <Box pad="medium" align="center">
-          <Beer size={size} />
+          <Fader>
+            <Beer size={size} />
+            <Dashboard size={size} />
+            <Wireframe size={size} />
+          </Fader>
         </Box>
         <Header
           label="Projédex"
-          summary={
-            <span>
-              Like a pokédex, but for your projects.
-            </span>
-          }
+          summary={<span>Like a pokédex, but for your projects.</span>}
         />
       </Box>
     )}
