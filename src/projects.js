b9c3d9aa230ce40f32d4c4725e6db2325ce9ebe1
Alex Lee <alex@alex-lee.site>
2018-11-27 20:29:58 -0800

    ✏️ Add more project information.

--- src/projects.js
+++ src/projects.js
@@ -1,4 +1,4 @@
-import { Github } from 'grommet-icons'
+import { CatalogOption, Github, Home } from 'grommet-icons'
 import { iconColors } from './constants'
 
 // Array of Project Information
@@ -8,6 +8,12 @@ export default [
     description: 'Like a pokédex, but for your projects.',
     color: '#6194eb',
     links: [
+      {
+        name: 'homepage',
+        link: 'https://www.google.com',
+        icon: Home,
+        color: iconColors.home
+      },
       {
         name: 'github',
         link: 'https://github.com/alexlee-dev/projedex',
@@ -19,44 +25,162 @@ export default [
   {
     title: 'grommet-patterns',
     description: 'Common patterns and layouts for applications using Grommet.',
-    color: '#7D4CDB'
+    color: '#7D4CDB',
+    links: [
+      {
+        name: 'homepage',
+        link: 'https://grommet-patterns.netlify.com/',
+        icon: Home,
+        color: iconColors.home
+      },
+      {
+        name: 'github',
+        link: 'https://github.com/alexlee-dev/grommet-patterns',
+        icon: Github,
+        color: iconColors.github
+      }
+    ]
   },
   {
     title: 'portfolio',
     description: "Alex Lee's Web Developer Portfolio.",
-    color: '#0a64a0'
+    color: '#0a64a0',
+    links: [
+      {
+        name: 'homepage',
+        link: 'https://www.alex-lee.site/',
+        icon: Home,
+        color: iconColors.home
+      },
+      {
+        name: 'github',
+        link: 'https://github.com/alexlee-dev/portfolio',
+        icon: Github,
+        color: iconColors.github
+      },
+      {
+        name: 'docs',
+        link: 'https://alexlee-portfolio-docs.netlify.com/',
+        icon: CatalogOption,
+        color: iconColors.docs
+      }
+    ]
   },
   {
     title: 'pickitt',
     description:
       'When you need you need a computer to just pick it, reach for Pickitt!',
-    color: '#0b5fff'
+    color: '#0b5fff',
+    links: [
+      {
+        name: 'homepage',
+        link: 'https://pickitt.netlify.com/',
+        icon: Home,
+        color: iconColors.home
+      },
+      {
+        name: 'github',
+        link: 'https://github.com/alexlee-dev/pickitt',
+        icon: Github,
+        color: iconColors.github
+      }
+    ]
   },
   {
     title: 'remembera',
     description: 'A knowledge bank.',
-    color: '#0b5fff'
+    color: '#0b5fff',
+    links: [
+      {
+        name: 'homepage',
+        link: 'https://www.remembera.info/',
+        icon: Home,
+        color: iconColors.home
+      },
+      {
+        name: 'github',
+        link: 'https://github.com/alexlee-dev/remembera',
+        icon: Github,
+        color: iconColors.github
+      }
+    ]
   },
   {
     title: 'trimettra',
     description: "Fitbit app using TriMet's developer API.",
-    color: '#e0e0e0'
+    color: '#e0e0e0',
+    links: [
+      {
+        name: 'github',
+        link: 'https://github.com/alexlee-dev/trimettra',
+        icon: Github,
+        color: iconColors.github
+      }
+    ]
   },
   {
     title: 'bitburner-community',
     description:
       'For inspiration, assistance, and discussion around the amazing cyperpunk-themed incremental RPG, Bitburner.',
-    color: '#00C781'
+    color: '#00C781',
+    links: [
+      {
+        name: 'homepage',
+        link: 'https://www.bitburnercommunity.site/',
+        icon: Home,
+        color: iconColors.home
+      },
+      {
+        name: 'github',
+        link: 'https://github.com/alexlee-dev/Bitburner-Community',
+        icon: Github,
+        color: iconColors.github
+      }
+    ]
   },
   {
     title: 'splague',
     description: 'Be prepared.',
-    color: '#ef860c'
+    color: '#ef860c',
+    links: [
+      {
+        name: 'homepage',
+        link: 'https://www.splague.com/',
+        icon: Home,
+        color: iconColors.home
+      },
+      {
+        name: 'github',
+        link: 'https://github.com/alexlee-dev/splague',
+        icon: Github,
+        color: iconColors.github
+      },
+      {
+        name: 'docs',
+        link: 'https://www.splague.com/docs/',
+        icon: CatalogOption,
+        color: iconColors.docs
+      }
+    ]
   },
   {
     title: 'reposier',
     description:
       "Tasty CLI on the outside, simple integration with GitHub's API on the inside.",
-    color: '#42b983'
+    color: '#42b983',
+    links: [
+      {
+        name: 'homepage',
+        link: 'https://www.reposier.com/',
+        icon: Home,
+        color: iconColors.home
+      },
+      {
+        name: 'github',
+        link: 'https://github.com/alexlee-dev/reposier',
+        icon: Github,
+        color: iconColors.github
+      }
+    ]
   }
 ]
