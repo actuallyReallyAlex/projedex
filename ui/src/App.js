d020ae4f7771c938358eeb4642a5ffd70a531706
alexlee-dev <alex@alexlee.dev>
2020-02-04 14:35:55 -0800

    Basic UI - Create User

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,26 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import request from "request";
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
+  const [name, setName] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+
+    request(
+      proxy + "https://projedex.herokuapp.com/users",
+      {
+        json: true,
+        body: { email, password, name },
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      },
+      (error, response, body) => {
+        if (error) {
+          return console.error(error);
+        }
+
+        setUser(body.user);
+        setToken(body.token);
+      }
+    );
+  };
+
+  useEffect(() => {
+    const localStorageUser = window.localStorage.getItem("user");
+    const localStorageToken = window.localStorage.getItem("token");
+
+    if (!localStorageUser) {
+      if (user) {
+        window.localStorage.setItem("user", JSON.stringify(user));
+      }
+    } else {
+      if (!user) {
+        const userObj = JSON.parse(localStorageUser);
+        setUser(userObj);
+        setName(userObj.name);
+        setEmail(userObj.email);
+      }
+    }
+
+    if (!localStorageToken) {
+      if (token) {
+        window.localStorage.setItem("token", token);
+      }
+    } else {
+      if (!token) {
+        setToken(localStorageToken);
+      }
+    }
+  }, [user, token]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Projedex (BASIC UI)</h1>
+      <pre>{JSON.stringify({ user, token, name, email }, null, 2)}</pre>
+      <form onSubmit={handleSubmit}>
+        {!user && (
+          <div id="login">
+            <label>Name</label>
+            <input
+              id="name"
+              onChange={e => setName(e.target.value)}
+              type="text"
+            />
+            <label>Email</label>
+            <input
+              id="email"
+              onChange={e => setEmail(e.target.value)}
+              type="text"
+            />
+            <label>Password</label>
+            <input
+              id="password"
+              onChange={e => setPassword(e.target.value)}
+              type="text"
+            />
+            <button type="submit">Login</button>
+          </div>
+        )}
+      </form>
     </div>
   );
-}
+};
 
 export default App;
