842c91cde77aca13f286e31e01b7f224d665bbc2
alexlee-dev <alex@alexlee.dev>
2020-02-05 14:06:53 -0800

    Project Tests

--- api/tests/fixtures/db.js
+++ api/tests/fixtures/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const User = require("../../src/models/user");
+const Project = require("../../src/models/project");
 
 const userOneId = new mongoose.Types.ObjectId();
 const userOne = {
@@ -28,10 +29,39 @@ const userTwo = {
   ]
 };
 
+const projectOne = {
+  _id: new mongoose.Types.ObjectId(),
+  name: "Project One",
+  owner: userOneId
+};
+
+const projectTwo = {
+  _id: new mongoose.Types.ObjectId(),
+  name: "Project Two",
+  owner: userOneId
+};
+
+const projectThree = {
+  _id: new mongoose.Types.ObjectId(),
+  name: "Project Three",
+  owner: userTwoId
+};
+
+const projectFour = {
+  _id: new mongoose.Types.ObjectId(),
+  name: "Project Four",
+  owner: userTwoId
+};
+
 const setupDatabase = async () => {
   await User.deleteMany();
+  await Project.deleteMany();
   await new User(userOne).save();
   await new User(userTwo).save();
+  await new Project(projectOne).save();
+  await new Project(projectTwo).save();
+  await new Project(projectThree).save();
+  await new Project(projectFour).save();
 };
 
 module.exports = {
@@ -39,5 +69,9 @@ module.exports = {
   userOne,
   userTwoId,
   userTwo,
+  projectOne,
+  projectTwo,
+  projectThree,
+  projectFour,
   setupDatabase
 };
