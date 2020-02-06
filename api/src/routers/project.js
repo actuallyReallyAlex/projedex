8bcb64b5281dfa8947d1dc9c67cb40d7d77acb2c
alexlee-dev <alex@alexlee.dev>
2020-02-06 13:46:28 -0800

    API - GitHub API Integration

--- api/src/routers/project.js
+++ api/src/routers/project.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = new express.Router();
 const Project = require("../models/project");
 const auth = require("../middleware/auth");
+const { Octokit } = require("@octokit/rest");
 
 router.post("/projects", auth, async (req, res) => {
   const project = new Project({ ...req.body, owner: req.user._id });
@@ -84,6 +85,45 @@ router.delete("/projects/:id", auth, async (req, res) => {
     res.send(project);
   } catch (e) {
     res.status(500).send();
+  }
+});
+
+router.get("/projects-import", auth, async (req, res) => {
+  try {
+    const octokit = new Octokit({ auth: req.user.gitHubPersonalAccessToken });
+    const userResponse = await octokit.request("/user");
+
+    const { login, public_repos } = userResponse.data;
+    const fields = [
+      { name: "gitHubLogin", value: login },
+      { name: "gitHubPublicRepoCount", value: public_repos }
+    ];
+
+    fields.forEach(({ name, value }) => {
+      if (value) {
+        req.user[name] = value;
+      } else {
+        req.user[name] = "";
+      }
+    });
+
+    await req.user.save();
+
+    const repoResponse = await octokit.request("/user/repos");
+    const repoData = repoResponse.data;
+    const ownedRepoData = repoData.filter(repo => repo.owner.login === login);
+
+    ownedRepoData.forEach(async ownedRepo => {
+      const project = new Project({
+        owner: req.user._id,
+        name: ownedRepo.name
+      });
+      await project.save();
+    });
+
+    res.status(201).send();
+  } catch (e) {
+    res.status(500).send({ error: e });
   }
 });
 
