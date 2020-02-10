de042ebcb3a45d22f3fe69bead381cac580887b2
alexlee-dev <alex@alexlee.dev>
2020-02-10 13:29:18 -0800

    Abstract apiDomain and proxy

--- ui/src/constants.js
+++ ui/src/constants.js
@@ -1,1 +1,6 @@
 export const proxy = "https://cors-anywhere.herokuapp.com/";
+
+export const apiDomain =
+  process.env.NODE_ENV === "development"
+    ? process.env.REACT_APP_API_DOMAIN
+    : proxy + process.env.REACT_APP_API_DOMAIN;
