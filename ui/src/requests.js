5390fdd590a2ea96416c52503ed5d858130bcd74
alexlee-dev <alex@alexlee.dev>
2020-02-10 13:30:03 -0800

    Ability to get repos, import specific/all repos

--- ui/src/requests.js
+++ ui/src/requests.js
@@ -339,4 +339,48 @@ export const saveAccessToken = (userData, accessToken, cb) =>
       }
     }
   );
-  
+
+export const getRepos = (userData, handleSetRepos) => {
+  request(
+    `${apiDomain}/gh-import`,
+    {
+      json: true,
+      method: "GET",
+      auth: {
+        bearer: userData.token
+      }
+    },
+    (error, response, body) => {
+      if (error) {
+        return console.error(error);
+      }
+
+      if (response.statusCode === 200) {
+        handleSetRepos(response.body);
+      }
+    }
+  );
+};
+
+export const importRepos = (userData, repos, handleSetProjects) => {
+  request(
+    `${apiDomain}/gh-import`,
+    {
+      json: true,
+      method: "POST",
+      auth: {
+        bearer: userData.token
+      },
+      body: { repos }
+    },
+    (error, response, body) => {
+      if (error) {
+        return console.error(error);
+      }
+
+      if (response.statusCode === 201) {
+        handleSetProjects(response.body.projects);
+      }
+    }
+  );
+};
