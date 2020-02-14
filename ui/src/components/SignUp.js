77d06afcc00a61a7d132b2ce4f05415f7cacc11a
alexlee-dev <alex@alexlee.dev>
2020-02-13 16:04:14 -0800

    Add Error and Loading states to SignUp

--- ui/src/components/SignUp.js
+++ ui/src/components/SignUp.js
@@ -1,24 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { createUser } from "../redux/actions/user";
 import { connect } from "react-redux";
-import { Button, Container, Form, Input } from "semantic-ui-react";
+import { Button, Container, Form, Input, Message } from "semantic-ui-react";
+import validator from "validator";
+import { setError, setLoading } from "../redux/actions/app";
 
-const SignUp = ({ handleBack, handleCreateUser }) => {
-  const [name, setName] = useState(null);
-  const [email, setEmail] = useState(null);
-  const [password, setPassword] = useState(null);
-  const [confirmPassword, setConfirmPassword] = useState(null);
+const SignUp = ({
+  error,
+  handleBack,
+  handleCreateUser,
+  handleError,
+  handleResetState,
+  loading
+}) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-  // TODO - ConfirmPassword functionality
+  useEffect(() => {
+    return () => handleResetState();
+  }, [handleResetState]);
 
   const handleSubmit = () => {
-    console.log({ confirmPassword });
+    if (validator.isEmpty(name)) {
+      return handleError("Please provide a name for the user.");
+    }
+
+    if (!validator.isEmail(email) || validator.isEmpty(email)) {
+      return handleError("Please enter a valid email address.");
+    }
+
+    if (validator.isEmpty(password) || validator.isEmpty(confirmPassword)) {
+      return handleError("Please enter a password.");
+    }
+
+    if (password !== confirmPassword) {
+      return handleError("Passwords do not match.");
+    }
+
     handleCreateUser(email, name, password);
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
+    <Form error={error.state} loading={loading} onSubmit={handleSubmit}>
+      <Message error header="Error" content={error.message} />
       <Form.Field
         control={Input}
         label="Name"
@@ -58,13 +85,27 @@ const SignUp = ({ handleBack, handleCreateUser }) => {
 };
 
 SignUp.propTypes = {
+  error: PropTypes.object.isRequired,
   handleBack: PropTypes.func.isRequired,
-  handleCreateUser: PropTypes.func.isRequired
+  handleCreateUser: PropTypes.func.isRequired,
+  handleError: PropTypes.func.isRequired,
+  handleResetState: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired
 };
+
+const mapStateToProps = ({ app }) => ({
+  error: app.error,
+  loading: app.loading
+});
 
 const mapDispatchToProps = dispatch => ({
   handleCreateUser: (email, name, password) =>
-    dispatch(createUser(email, name, password))
+    dispatch(createUser(email, name, password)),
+  handleError: error => dispatch(setError({ state: true, message: error })),
+  handleResetState: () => {
+    dispatch(setError({ state: null, message: "" }));
+    dispatch(setLoading(false));
+  }
 });
 
-export default connect(null, mapDispatchToProps)(SignUp);
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
