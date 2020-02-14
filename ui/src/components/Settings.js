51d2bce5ee0bc892cf437f3ab162e258f939461e
alexlee-dev <alex@alexlee.dev>
2020-02-13 22:15:57 -0800

    Create Settings

---
+++ ui/src/components/Settings.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Placeholder } from "semantic-ui-react";
+
+const Settings = () => {
+  return (
+    <Placeholder>
+      <h2>Settings</h2>
+      <Placeholder.Header image>
+        <Placeholder.Line />
+        <Placeholder.Line />
+      </Placeholder.Header>
+      <Placeholder.Paragraph>
+        <Placeholder.Line />
+        <Placeholder.Line />
+        <Placeholder.Line />
+        <Placeholder.Line />
+      </Placeholder.Paragraph>
+    </Placeholder>
+  );
+};
+
+export default Settings;
