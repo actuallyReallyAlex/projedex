f4bee23527e28f2b0057e576fe0a08d3d5347d7f
alexlee-dev <alex@alexlee.dev>
2020-02-19 11:51:12 -0800

    Remove ui-old

--- ui-old/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import AppRouter from './routers/AppRouter.js'
-
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<AppRouter />, div)
-  ReactDOM.unmountComponentAtNode(div)
-})
