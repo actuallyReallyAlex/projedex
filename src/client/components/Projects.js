a632c18f626a95276385a2388399e0298fde4bce
alexlee-dev <alex@alexlee.dev>
2020-03-20 09:50:55 -0700

    Conditionally render Integrate button based on accessToken

--- src/client/components/Projects.js
+++ src/client/components/Projects.js
@@ -5,13 +5,15 @@ import { connect } from 'react-redux'
 import { integrateWithGitHub } from '../redux/actions/app'
 
 // TODO - Make this look nice
-const Projects = ({ handleIntegrateWithGitHub, projectData }) => {
+const Projects = ({ handleIntegrateWithGitHub, isIntegratedWithGitHub, projectData }) => {
   return (
     <div>
       <Header as="h2">Projects</Header>
-      <Button onClick={handleIntegrateWithGitHub}>
-        <Icon name="github" /> Integrate with Github
-      </Button>
+      {!isIntegratedWithGitHub && (
+        <Button onClick={handleIntegrateWithGitHub}>
+          <Icon name="github" /> Integrate with Github
+        </Button>
+      )}
       {projectData.map(project => (
         <Segment key={project._id} raised>
           <Header as="h3">{project.name}</Header>
@@ -24,10 +26,12 @@ const Projects = ({ handleIntegrateWithGitHub, projectData }) => {
 
 Projects.propTypes = {
   handleIntegrateWithGitHub: PropTypes.func.isRequired,
+  isIntegratedWithGitHub: PropTypes.bool.isRequired,
   projectData: PropTypes.array.isRequired
 }
 
-const mapStateToProps = ({ projects }) => ({
+const mapStateToProps = ({ projects, user }) => ({
+  isIntegratedWithGitHub: user.userData.user.accessToken,
   projectData: projects.projectData
 })
 
